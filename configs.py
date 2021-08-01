d9ad81078d39a2275e27f8724190fc65b07b8f2f
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-08-01 12:10:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,7 +14,8 @@ class Config(object):
     BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
     START_TEXT = """
-Hi, I am Channel Broadcast Footer Bot!
+Hi, {}
+I am Channel Custom Caption Bot!
 
-I can add footer to Channel Media Messages. Just add me to the channel as Admin with all rights and setup /settings !!
+I can add caption to Channel Media Messages. Just add me to the channel as Admin with all rights and setup /settings !!
 """
