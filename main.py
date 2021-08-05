9d2a16a66bfdef21ffa6cc4a6295588013ca656a
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-08-05 23:32:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,10 +54,10 @@ async def _start(bot: Client, m: Message):
 
 @AHBot.on_message(filters.private & filters.command("settings"))
 async def _settings(bot: Client, event: Message):
-    if m.from_user.id in Config.BANNED_USERS:
+    if event.from_user.id in Config.BANNED_USERS:
         await bot.delete_messages(
-            chat_id=m.chat.id,
-            message_ids=m.message_id,
+            chat_id=event.chat.id,
+            message_ids=event.message_id,
             revoke=True
         )
         return
@@ -68,6 +68,9 @@ async def _settings(bot: Client, event: Message):
 
 @AHBot.on_message(filters.channel & (filters.video | filters.document) & ~filters.edited & ~filters.private)
 async def add_footer(bot: Client, event: Message):
+    if int(event.chat.id) in Config.BANNED_CHANNELS:
+        await bot.leave_chat(event.chat.id)
+        return
     on_event = await db.find_user_id(event.chat.id)
     if on_event is None:
         return
@@ -84,12 +87,8 @@ async def add_footer(bot: Client, event: Message):
 
 @AHBot.on_message(filters.channel & filters.text & ~filters.edited & ~filters.private, group=-1)
 async def add_text_footer(bot: Client, event: Message):
-    if event.from_user.id in Config.BANNED_USERS:
-        await bot.delete_messages(
-            chat_id=event.chat.id,
-            message_ids=event.message_id,
-            revoke=True
-        )
+    if int(event.chat.id) in Config.BANNED_CHANNELS:
+        await bot.leave_chat(event.chat.id)
         return
     on_event = await db.find_user_id(event.chat.id)
     if on_event is None:
@@ -108,6 +107,9 @@ async def add_text_footer(bot: Client, event: Message):
 
 @AHBot.on_message(filters.channel & filters.photo & ~filters.edited & ~filters.private)
 async def add_text_footer(bot: Client, event: Message):
+    if int(event.chat.id) in Config.BANNED_CHANNELS:
+        await bot.leave_chat(event.chat.id)
+        return
     on_event = await db.find_user_id(event.chat.id)
     if on_event is None:
         return
